3c33ee25cd72cd99fc0b969243a408dca4353338
cruzcstll99 <42954446+cruzcstll99@users.noreply.github.com>
2024-10-09 13:26:26 -0500

    Added a gif

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce))");
+const details = document.querySelector(".object-and-details > details");
 
+if (mediaQuery.matches) {
+  details.removeAttribute("open");
+}
