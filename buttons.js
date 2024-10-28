e6011b2b8ae1e9ed3dfd23d9f2cd0f9a99479ed1
cruzcstll99 <42954446+cruzcstll99@users.noreply.github.com>
2024-10-28 11:43:36 -0500

    Update button.js
    Fixed teh distortion to the pcs term

--- buttons.js
+++ buttons.js
@@ -47,11 +47,11 @@ function switchtraceImage() {
 
 
 const smallpcs = ['Images/PCS_zeta_small.gif','Images/PCS_delta_small.gif','Images/PCS_E_small.gif']
-let currentPCSImage  =0;
+let currentsmallPCSImage  =0;
 function switchsmallpcsImage() {
-    currentPCSImage = (currentPCSImage + 1) % smallpcs.length;
-    document.getElementById('smallPCS').src = smallpcs[currentPCSImage];
-    document.getElementById("smallpcs_text").innerHTML = L_funcs[currentPCSImage];
+    currentsmallPCSImage = (currentsmallPCSImage + 1) % smallpcs.length;
+    document.getElementById('smallPCS').src = smallpcs[currentsmallPCSImage];
+    document.getElementById("smallpcs_text").innerHTML = L_funcs[currentsmallPCSImage];
 }
 let currrentLfunc = 0;
 function switchL() {
